010eea9258a881eeb20469c951148389f3ecc340
Busra1Gunes <busragns1999@hotmail.com>
2024-08-27 17:51:03 +0300

    11.0 created in WebAPI

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]   //ATTIRIBUTE:bir class ile ilgili bilgi verme imzalama yönetemidir
+    //Controller isimleri çogul yazılır 
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public List<Product> Get()
+        {
+            return new List<Product>
+            {
+                new Product { ProductId = 1, ProductName = "Elma" },
+                new Product { ProductId = 2, ProductName = "Armut" }
+            };
+           
+        }
+
+    }
+}
